4cb7194cd74cde75d8c7226f0991e1194e394d1c
fullstack2500-dotcom <fullstack2500@gmail.com>
2026-01-12 10:15:34 +0800

    Update authentication.ts

--- src/controller/authentication.ts
+++ src/controller/authentication.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from "express";
 import User from "../model/User";
 import bcrypt from "bcryptjs";
+import { RegisterSchema } from "../validator/authentication";
 
 // Register The User:
 export const Register = async (req: Request, res: Response) => {
-  const { name, email, password, role } = req.body;
+  // Validate user information:
+  const validatedData = RegisterSchema.safeParse(req.body)
+  if (validatedData.error) {
+
+    const errors = JSON.parse(validatedData.error.message)
+    return res.status(400).json({ success: false, message: errors[0].message })
+  }
+
+  const { name, email, password, role } = validatedData.data;
 
   try {
     const hash = await bcrypt.hash(password, 12);
