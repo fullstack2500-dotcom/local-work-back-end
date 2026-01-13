d3574ef026464ccc53f7e0ba3236d6c2e7f9a09e
fullstack2500-dotcom <fullstack2500@gmail.com>
2026-01-13 14:37:16 +0800

    Update User.ts

--- src/model/User.ts
+++ src/model/User.ts
@@ -4,14 +4,16 @@ export interface IUser extends Document {
   name: string,
   email: string,
   password: string,
-  role: string
+  role: string,
+  profile: string
 }
 
 const UserSchema: Schema = new Schema({
   name: { type: String, required: [true, "Name is required"], unique: [true, "Name is already in use"] },
   email: { type: String, required: [true, "Email is required"], unique: [true, "Email is already in use"] },
   password: { type: String, required: [true, "Password is required"] },
-  role: { type: String, enum: ["worker", "employer", "admin"], default: "worker" }
+  role: { type: String, enum: ["worker", "employer", "admin"], default: "worker" },
+  profile: { type: String }
 }, { timestamps: true })
 
 export default model<IUser>("User", UserSchema)
