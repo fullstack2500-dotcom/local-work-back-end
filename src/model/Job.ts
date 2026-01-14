5b5a6cdaed6feb34b744fc1d8307316ed13ccce1
fullstack2500-dotcom <fullstack2500@gmail.com>
2026-01-14 13:27:15 +0800

    Create Job.ts

---
+++ src/model/Job.ts
@@ -0,0 +1,28 @@
+import { Document, Schema, model, ObjectId } from "mongoose";
+
+export interface IJob extends Document {
+  title: string,
+  description: string,
+  requirements: [],
+  employerInformation: string,
+  rating: Number,
+  location: ObjectId,
+  salaryPerDay: Number,
+  type: string,
+  status: string,
+  review: string
+}
+
+const JobSchema: Schema = new Schema({
+  title: { type: String, required: [true, "Title is required"], unique: [true, "Title already exists"] },
+  description: { type: String, required: [true, "Description is required"] },
+  requirements: { type: [], default: [] },
+  employerInformation: { type: String, required: [true, "Employer Information is required"] },
+  rating: { type: Number, default: 0 },
+  location: { type: Schema.ObjectId },
+  salaryPerDay: { type: Number, required: [true, "Salary is required"] },
+  type: { type: String, enum: ["ongoing", "approved"], default: "ongoing" },
+  review: { type: String, default: "" }
+})
+
+export default model<IJob>("Job", JobSchema)
