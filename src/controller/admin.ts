4fa3938a28ed3509368f16198e3f55ed968f2ea6
fullstack2500-dotcom <fullstack2500@gmail.com>
2026-01-14 14:07:30 +0800

    Update admin.ts

--- src/controller/admin.ts
+++ src/controller/admin.ts
@@ -1,9 +1,27 @@
 import User from "../model/User";
 import { Request, Response } from "express";
+import { TotalWorkerSchema } from "../validator/admin";
 
 export const TotalWorkers = async (req: Request, res: Response) => {
   try {
-    const noOfWorkers = await User.find({ })
+    const validatedRole = TotalWorkerSchema.safeParse(req.query)
+    if (validatedRole.error) {
+      const errors = JSON.parse(validatedRole.error.message)
+      return res.status(400).json({
+
+        success: false,
+        message: errors[0].message
+      })
+    }
+
+    const { role } = validatedRole.data
+
+    const noOfWorkers = await User.find({ role })
+
+    return res.status(200).json({
+      success: true,
+      totalWorkers: noOfWorkers.length
+    })
   } catch (error) {
     console.error(error)
 
