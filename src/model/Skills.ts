9c09a43718182be4efe64f97c1ed7936d8705e69
fullstack2500-dotcom <fullstack2500@gmail.com>
2026-01-17 11:35:16 +0800

    Update Skills.ts

--- src/model/Skills.ts
+++ src/model/Skills.ts
@@ -3,13 +3,11 @@ import { Document, ObjectId, Schema, model } from "mongoose";
 export interface ISkill extends Document {
   title: string,
   fileURL: string,
-  user: ObjectId
 }
 
 const SkillSchema: Schema = new Schema({
   title: { type: String, required: [true, "Skill is required"] },
-  fileURL: { type: String, required: [true, "File URL is required"]},
-  user: { type: Schema.ObjectId, ref: 'User', required: [true, "User ID is required"] }
+  fileURL: { type: String, required: [true, "File URL is required"]}
 }, { timestamps: true })
 
 export default model<ISkill>("Skill", SkillSchema)
