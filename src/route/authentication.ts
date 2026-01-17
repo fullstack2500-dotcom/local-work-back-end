ae4040043bf584cb44e9b0d114637f2effc06542
fullstack2500-dotcom <fullstack2500@gmail.com>
2026-01-18 05:25:13 +0800

    Updated Packages

--- src/route/authentication.ts
+++ src/route/authentication.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { Register, Login, TotalWorkers } from "../controller/authentication";
+import rateLimit from "express-rate-limit";
 
 const router = Router()
 
@@ -7,7 +8,26 @@ const router = Router()
 router.get("/local", TotalWorkers)
 
 // Register and Login the User:
-router.post("/register", Register)
+router.post("/register",
+  rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 10,
+    message: "Too many failed attempts, please try again after 1 minute",
+    skipSuccessfulRequests: true
+  }),
+  rateLimit({
+    windowMs: 2 * 60 * 1000,
+    max: 10 + 4,
+    message: "Too many failed attempts, please try again after 2 minutes",
+    skipSuccessfulRequests: true
+  }),
+  rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 10 + 4 + 1,
+    message: "Too many failed attempts, please try again after 5 minutes",
+    skipSuccessfulRequests: true
+  }),
+Register)
 router.post("/login", Login)
 
 export default router
