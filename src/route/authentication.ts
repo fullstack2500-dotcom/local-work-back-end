087e160da418a5591cac24f31d6fc5072d30646e
fullstack2500-dotcom <fullstack2500@gmail.com>
2026-01-18 17:28:38 +0800

    Update authentication.ts

--- src/route/authentication.ts
+++ src/route/authentication.ts
@@ -7,7 +7,7 @@ const router = Router()
 // Display the total workers:
 router.get("/local", TotalWorkers)
 
-// Register and Login the User:
+// Register User:
 router.post("/register",
   rateLimit({
     windowMs: 1 * 60 * 1000,
@@ -48,6 +48,55 @@ router.post("/register",
   }),
 
 Register)
-router.post("/login", Login)
+
+
+// Login User:
+router.post(
+  "/login",
+
+  rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 10,
+    message: "Too many failed attempts, please try again after 1 minute.",
+    skipSuccessfulRequests: true
+  }),
+
+  rateLimit({
+    windowMs: 3 * 60 * 1000,
+    max: 10 + 4,
+    message: "Too many failed attempts, please try again after 3 minutes.",
+    skipSuccessfulRequests: true
+  }),
+
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 10 + 4 + 2,
+    message: "Too many failed attempts, please try again after 15 minutes.",
+    skipSuccessfulRequests: true
+  }),
+
+  rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 10 + 4 + 2 + 1,
+    message: "Too many failed attempts, please try again after 1 hr.",
+    skipSuccessfulRequests: true
+  }),
+  
+  rateLimit({
+    windowMs: 24 * 60 * 60 * 1000,
+    max: 10 + 4 + 2 + 1 + 1,
+    message: "Too many failed attempts, please try again after 24 hrs.",
+    skipSuccessfulRequests: true
+  }),
+
+  rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 20,
+    message: "Too many login attempts, please try again after 1 hr.",
+    skipFailedRequests: true
+  }),
+
+  Login
+)
 
 export default router
