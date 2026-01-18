9e801d606df477a6df0fdf14b0fec978409ba8f9
fullstack2500-dotcom <fullstack2500@gmail.com>
2026-01-18 17:24:11 +0800

    Updated Rate Limiting and Slow Down for Register

--- src/route/authentication.ts
+++ src/route/authentication.ts
@@ -12,21 +12,41 @@ router.post("/register",
   rateLimit({
     windowMs: 1 * 60 * 1000,
     max: 10,
-    message: "Too many failed attempts, please try again after 1 minute",
+    message: "Too many failed attempts, please try again after 1 minute.",
     skipSuccessfulRequests: true
   }),
   rateLimit({
-    windowMs: 2 * 60 * 1000,
+    windowMs: 3 * 60 * 1000,
     max: 10 + 4,
-    message: "Too many failed attempts, please try again after 2 minutes",
+    message: "Too many failed attempts, please try again after 3 minutes.",
     skipSuccessfulRequests: true
   }),
   rateLimit({
-    windowMs: 5 * 60 * 1000,
-    max: 10 + 4 + 1,
-    message: "Too many failed attempts, please try again after 5 minutes",
+    windowMs: 15 * 60 * 1000,
+    max: 10 + 4 + 2,
+    message: "Too many failed attempts, please try again after 15 minutes.",
     skipSuccessfulRequests: true
   }),
+  rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 10 + 4 + 2 + 1,
+    message: "Too many failed attempts, please try again after 1 hr.",
+    skipSuccessfulRequests: true
+  }),
+  rateLimit({
+    windowMs: 24 * 60 * 60 * 1000,
+    max: 10 + 4 + 2 + 1 + 1,
+    message: "Too many failed attempts, please try again after 24 hrs.",
+    skipSuccessfulRequests: true
+  }),
+
+  rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 20,
+    message: "Too many accounts were created, please try again after 1 hr.",
+    skipFailedRequests: true
+  }),
+
 Register)
 router.post("/login", Login)
 
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import protectedRoutes from "./route/protected"
 import adminRoutes from "./route/admin"
 import helmet from "helmet";
 import cors from "cors"
+import slowDown from "express-slow-down";
+import rateLimit from "express-rate-limit";
 
 connectDB()
 
@@ -16,9 +18,26 @@ app.use(cors())
 
 app.use(express.json())
 
+// Rate Limit:
+const rateLimiter = rateLimit({
+  windowMs: 2 * 60 * 1000,
+  max: 500,
+  message: "Too many requests, please try again after 2 minutes"
+})
+
+// Slow Down:
+const speedLimit = slowDown({
+  windowMs: 15 * 60 * 1000,
+  delayAfter: 40,
+  delayMs: (hits) => hits * 120
+})
+
+// Allow app to use rate limiter and speed limiter:
+app.use("/api", rateLimiter, speedLimit)
+
 // Middlewares for authentication and protected and admin:
 app.use("/api/auth", authRouter)
-app.use("/api", protectedRoutes)
+app.use("/api/pro", protectedRoutes)
 app.use("/api/admin", adminRoutes)
 
 const port = process.env.PORT || 5000
