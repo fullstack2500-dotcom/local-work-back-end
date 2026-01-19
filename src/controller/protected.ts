a96ab6ee00bc17a5b7dd88a869a7b0d9bafb3115
fullstack2500-dotcom <fullstack2500@gmail.com>
2026-01-19 15:58:37 +0800

    Created Logout Controller

--- src/controller/protected.ts
+++ src/controller/protected.ts
@@ -1,8 +1,30 @@
 import { Request, Response } from "express";
+import User from "../model/User";
 
+// Dashboard:
 export const Dashboard = async (req: Request, res: Response) => {
   return res.status(200).json({
     success: true,
     message: "Dashboard Info"
   })
+}
+
+
+
+
+// Log Out Controller:
+export const LogOut = async (req: Request, res: Response) => {
+  try {
+    return res
+              .clearCookie("token")
+              .status(200).json({
+                success: true,
+                message: "Successfully Logged Out!"
+              })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error"
+    })
+  }
 }
--- src/route/protected.ts
+++ src/route/protected.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { Dashboard } from "../controller/protected";
+import { Dashboard, LogOut } from "../controller/protected";
 import authorized from "../middleware/authorized";
 
 const router = Router()
 
 // GET Requests:
 router.get("/dashboard", authorized, Dashboard)
+
+// POST Requests:
+router.post("/logout", authorized, LogOut)
 
 export default router
