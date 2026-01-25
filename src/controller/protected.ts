b592edd084e0115fa09cc00a526173311718ade9
fullstack2500-dotcom <fullstack2500@gmail.com>
2026-01-25 14:50:34 +0800

    Update protected.ts

--- src/controller/protected.ts
+++ src/controller/protected.ts
@@ -34,9 +34,9 @@ export const createJob = async (req: Request, res: Response) => {
         return res.status(400).json({ success: false, message: error[0].message })
   }
 
-  // Link - https://medium.com/@ferrosful/nodejs-security-unleashed-exploring-xss-attack-8d3a61a01a09:
-  // Sanitize XSS:
   let { title, description } = validatedJobData.data
+
+  // Sanitize XSS: Link - https://medium.com/@ferrosful/nodejs-security-unleashed-exploring-xss-attack-8d3a61a01a09:
   title = filterXSS(title, { whiteList: {}, stripIgnoreTag: true, stripIgnoreTagBody: ['script'] })
   description = filterXSS(description, { whiteList: {}, stripIgnoreTag: true, stripIgnoreTagBody: ['script'] })
 
