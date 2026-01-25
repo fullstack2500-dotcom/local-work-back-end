36b0529b8b2d4910214461960b325e1cfb45e1d8
fullstack2500-dotcom <fullstack2500@gmail.com>
2026-01-25 09:53:44 +0800

    Update Job.ts

--- src/model/Job.ts
+++ src/model/Job.ts
@@ -1,31 +1,21 @@
-import { Document, Schema, model, ObjectId } from "mongoose";
+import { Document, Schema, ObjectId, model } from "mongoose";
 
 export interface IJob extends Document {
   title: string,
-  description: string,
-  postedBy: ObjectId, // The ObjectId of the Employer that made this Job
-  rating: Number,
-  location: ObjectId,
-  salaryPerDay: Number,
-  hours: Number,
+  postedBy: ObjectId,
   type: string,
-  status: string,
-  review: string,
-  isAvailable: Boolean
+  salaryPerDay: Number,
+  hoursNeeded: Number,
+  description: string
 }
 
 const JobSchema: Schema = new Schema({
-  title: { type: String, required: [true, "Title is required"], unique: ["Title is already exists"] },
-  description: { type: String, required: [true, "Description is required"] },
-  postedBy: { type: Schema.Types.ObjectId, ref: "User", required: [true, "Please enter a user"] },
-  rating: { type: Number, default: 0 },
-  location: { type: Schema.Types.ObjectId },
-  salaryPerDay: { type: Number, required: [true, "Salary is required"] },
-  hours: { type: Number, required: [true, "Hours per day is required"] },
+  title: { type: String, required: [true, "Title is required"], unique: [true, "Title must be unique"] },
+  postedBy: { type: Schema.Types.ObjectId, required: [true, "Employer ID is required"] },
   type: { type: String, enum: ["Part Time", "Full Time"], default: "Full Time" },
-  status: { type: String, enum: ["ongoing", "approved"], default: "ongoing" },
-  review: { type: String, default: "" },
-  isAvailable: { type: Boolean, default: true }
-}, { timestamps: true })
+  salaryPerDay: { type: Number, required: [true, "Salary Per Day is required"] },
+  hoursNeeded: { type: Number, required: [true, "Hours Needed is required"] },
+  description: { type: String, required: [true, "Description is required"] }
+})
 
 export default model<IJob>("Job", JobSchema)
